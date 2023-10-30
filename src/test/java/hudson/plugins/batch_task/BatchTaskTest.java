07492690ea426ce924f2064ff5b1ed915ed8615e
Jerome Lacoste <jerome.lacoste@gmail.com>
2023-10-30 14:20:03 +0100

    Adjust expected value for TASK_ID for test to pass.
    Needed as we are scheduling the build twice.
    Unsure why the test was passing with a different value some years ago.

--- src/test/java/hudson/plugins/batch_task/BatchTaskTest.java
+++ src/test/java/hudson/plugins/batch_task/BatchTaskTest.java
@@ -102,8 +102,8 @@ public class BatchTaskTest {
         assertEquals("Cause type", UserCause.class.getName(),
                 ca.getCauses().get(0).getClass().getName());
         String log = Util.loadFile(run.getLogFile());
-        assertTrue("Expected 1-1:global-property:bar:anonymous in task output: " + log,
-                log.contains("1-1:global-property:bar:anonymous"));
+        assertTrue("Expected 2-1:global-property:bar:anonymous in task output: " + log,
+                log.contains("2-1:global-property:bar:anonymous"));
     }
 
     /**
